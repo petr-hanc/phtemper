c8fc672beee0e37c643fa922d387f9852aa1e3c6
petr-hanc <hancp@centrum.cz>
2021-08-22 23:00:20 +0200

    beginning of testing

--- src/main/java/phtemper/PhtemperApplication.java
+++ src/main/java/phtemper/PhtemperApplication.java
@@ -1,11 +1,16 @@
 package phtemper;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PhtemperApplication {
+public class PhtemperApplication implements CommandLineRunner {
 	
     @Autowired
     TemperRepository repository;
@@ -13,6 +18,13 @@ public class PhtemperApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PhtemperApplication.class, args);
+	}
+	
+	@Override
+	public void run(String... args) throws Exception {
+		//List<Temper> tempers = new ArrayList<Temper>();
+		//tempers.add(new Temper(LocalDateTime.parse("2021-08-01T10:05"), 20.3F));
+		//repository.saveAll(tempers);
 	}
 
 }
---
+++ src/test/java/phtemper/PeriodComputeIntegrTest.java
@@ -0,0 +1,14 @@
+package phtemper;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PeriodComputeIntegrTest {
+
+	@Test
+	public void testLongestPeriod() {
+		fail("Not yet implemented");
+	}
+
+}
