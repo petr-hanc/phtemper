e92452873fa9e1e2a8df5ac0878c793604f9a692
petr-hanc <hancp@centrum.cz>
2021-08-23 21:28:38 +0200

    longestPeriod() - add searching multiple periods

--- src/main/java/phtemper/PeriodCompute.java
+++ src/main/java/phtemper/PeriodCompute.java
@@ -1,5 +1,8 @@
 package phtemper;
 
+import java.time.Period;
+import java.time.chrono.ChronoPeriod;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Collections;
 
@@ -15,31 +18,45 @@ public class PeriodCompute {
 	/** Returns longest period (starting date and ending date) with all temperatures between lowTemp and hiTemp (included).
 	 * If no such temperature is found then returns null  */
 	public PeriodD longestPeriod(Float lowTemp, Float hiTemp) {
-		Temper oldestInRange = null, newestInRange = null;
-		PeriodD period = new PeriodD();
+		PeriodD periodMax = null;		// longest period in temperature range
+		long periodMaxLength = -1;		// length in days
+		Temper oldestInRange = null;
+		Temper newestInRange = null;
+		boolean withinPeriod = false;	// true if period was found and hasn't finished yet
 		
 		if (lowTemp == null || hiTemp == null || lowTemp > hiTemp)
 			return null;
 		ArrayList<Temper> sortedTemps = new ArrayList<Temper>(repository.findAll());
 		Collections.sort(sortedTemps);	// sort by date
 		for (Temper temper: sortedTemps) {
-			if (temper.getTemper() >= lowTemp && temper.getTemper() <= hiTemp) {
-				if (oldestInRange == null) {
+			if (temper.getTemper() >= lowTemp && temper.getTemper() <= hiTemp) {	// temper. is in range
+				if (oldestInRange == null) {	// new period has started
 					oldestInRange = temper;
 					newestInRange = temper;
+					withinPeriod = true;
 				}
-				else 
+				else 		// period continues
 					newestInRange = temper;
+			} else {	// temper. out of range
+				if (withinPeriod == true) {		// period has ended
+					PeriodD period = new PeriodD();
+					ChronoPeriod periodChrono;	// interval between dates
+					long periodLength = 0;		// length in days
+					period.setFromDate(oldestInRange.getTimeStamp().toLocalDate());
+					period.setToDate(newestInRange.getTimeStamp().toLocalDate());
+					periodChrono = Period.between(period.getFromDate(), period.getFromDate());
+					periodLength = periodChrono.get(ChronoUnit.DAYS);
+					if (periodLength > periodMaxLength) {	// this period has been the longest one
+						periodMax = period;
+						periodMaxLength = periodLength;
+					}
+					withinPeriod = false;
+					oldestInRange = null;
+					newestInRange = null;
+				}
 			}
 		}
-		if (oldestInRange == null)	// no temperature in range found
-			return null;
-		else {
-			period.setFromDate(oldestInRange.getTimeStamp().toLocalDate());
-			period.setToDate(newestInRange.getTimeStamp().toLocalDate());
-			return period;
-		}
-			
+		return periodMax;
 	}
 
-}
+}	// class
