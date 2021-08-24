5504bd26783a4d0f1bc617bb5fddb8881f877735
petr-hanc <hancp@centrum.cz>
2021-08-24 23:57:03 +0200

    trying run H2 database with TCP server, not successful

--- src/main/java/phtemper/PhtemperApplication.java
+++ src/main/java/phtemper/PhtemperApplication.java
@@ -1,30 +1,44 @@
 package phtemper;
 
+import java.beans.PropertyVetoException;
+import java.sql.SQLException;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import javax.sql.DataSource;
+
+import org.h2.tools.Server;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+
+import com.zaxxer.hikari.HikariDataSource;
 
 @SpringBootApplication
-public class PhtemperApplication implements CommandLineRunner {
+public class PhtemperApplication extends SpringBootServletInitializer /* implements CommandLineRunner */ {
 	
     @Autowired
     TemperRepository repository;
     @Autowired
     PeriodCompute periodCompute;
-	
-
+    @Autowired
+    private Environment env;
+    
 	public static void main(String[] args) {
 		SpringApplication.run(PhtemperApplication.class, args);
 	}
 	
-	@Override
+	//@Override
 	public void run(String... args) throws Exception {
+		
 		/*
+		repository.deleteAll();
 		List<Temper> tempers = new ArrayList<Temper>();
 		tempers.add(new Temper(LocalDateTime.parse("2021-08-01T11:30:00"), 25.4F));
 		tempers.add(new Temper(LocalDateTime.parse("2021-07-31T04:13:00"), 9.8F));
@@ -35,6 +49,24 @@ public class PhtemperApplication implements CommandLineRunner {
 		tempers.add(new Temper(LocalDateTime.parse("2021-08-20T11:40:00"), 35F));
 		repository.saveAll(tempers);
 		*/
+		
 	}
+	
+	@Bean(destroyMethod = "close")
+	public DataSource dataSource(Optional<Server> h2Server) throws PropertyVetoException {
+	    HikariDataSource ds = new HikariDataSource();
+	    ds.setDriverClassName(env.getProperty("db.driver"));
+	    ds.setJdbcUrl(env.getProperty("db.url"));
+	    ds.setUsername(env.getProperty("db.user"));
+	    ds.setPassword(env.getProperty("db.pass"));
+	    return ds;
+	}
+	
+	@Bean(initMethod = "start", destroyMethod = "stop")
+    public Server h2Server() throws SQLException {
+		Server srv = Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9092");
+		return srv;
+        //return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9092");
+    }
 
 }
