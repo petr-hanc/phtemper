073b81b038cdc60904bea2a78bbc24e5ab6e128e
petr-hanc <hancp@centrum.cz>
2021-08-28 19:03:16 +0200

    PeriodComputeIntegrTest - better test data and added asserts

--- src/test/java/phtemper/PeriodComputeIntegrTest.java
+++ src/test/java/phtemper/PeriodComputeIntegrTest.java
@@ -1,7 +1,10 @@
 package phtemper;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.fail;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.ArrayList;
@@ -16,6 +19,8 @@ import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
+
+/** Integration test of PeriodCompute - longestPeriod() and longestPeriodWithTime() */
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestPropertySource(locations = "classpath:application-integrationtest.properties")
@@ -35,9 +40,12 @@ public class PeriodComputeIntegrTest {
 		tempers.add(new Temper(LocalDateTime.parse("2021-07-31T04:13:00"), 9.8F));
 		tempers.add(new Temper(LocalDateTime.parse("2021-07-31T09:09:00"), 16.8F));
 		tempers.add(new Temper(LocalDateTime.parse("2021-08-03T05:15:00"), 5.0F));
-		tempers.add(new Temper(LocalDateTime.parse("2021-08-05T13:20:00"), 17F));
-		tempers.add(new Temper(LocalDateTime.parse("2021-08-15T11:40:00"), 17F));
-		tempers.add(new Temper(LocalDateTime.parse("2021-08-20T11:40:00"), 35F));
+		tempers.add(new Temper(LocalDateTime.parse("2021-08-04T08:00:00"), 15.0F));
+		tempers.add(new Temper(LocalDateTime.parse("2021-08-13T12:00:00"), 25.0F));
+		tempers.add(new Temper(LocalDateTime.parse("2021-08-13T12:00:01"), 25.01F));
+		tempers.add(new Temper(LocalDateTime.parse("2021-08-15T13:20:00"), 17F));
+		tempers.add(new Temper(LocalDateTime.parse("2021-08-25T12:00:00"), 17F));
+		tempers.add(new Temper(LocalDateTime.parse("2021-08-27T11:30:00"), 35F));
 		repository.saveAll(tempers);
     }
 
@@ -47,15 +55,16 @@ public class PeriodComputeIntegrTest {
 		System.out.println(repository.findAll());
 		PeriodD period = periodCompute.longestPeriod(15F, 25F);
 		System.out.println(period);
-		fail("Not yet implemented");
+		assertThat(period, equalTo(new PeriodD(LocalDate.parse("2021-08-15"), LocalDate.parse("2021-08-25"))));
 	}
 	
 	@Test
 	public void testLongestPeriodWithTime() {
 		System.out.println("testLongestPeriodWithTime()");
 		//System.out.println(repository.findAll());
-		PeriodD period = periodCompute.longestPeriodWithTime(15F, 25F, LocalTime.parse("08:00"), LocalTime.parse("11:40"));		
+		PeriodD period = periodCompute.longestPeriodWithTime(15F, 25F, LocalTime.parse("08:00"), LocalTime.parse("12:00"));		
 		System.out.println(period);
+		assertThat(period, equalTo(new PeriodD(LocalDate.parse("2021-08-04"), LocalDate.parse("2021-08-25"))));
 	}
 	
 	/*
