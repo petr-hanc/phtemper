10ff274bf8bec4934876e9fbc8e7d0aebfbd6333
petr-hanc <hancp@centrum.cz>
2021-08-30 21:43:30 +0200

    tune TemperTest

--- src/test/java/phtemper/TemperTest.java
+++ src/test/java/phtemper/TemperTest.java
@@ -1,7 +1,6 @@
 package phtemper;
 
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 import java.time.LocalDateTime;
 
@@ -13,7 +12,7 @@ public class TemperTest {
 	public void compareTest() {
 		Temper temper1 = new Temper(LocalDateTime.parse("2021-08-31T08:00:00"), -10.5F);
 		Temper temper2 = new Temper(LocalDateTime.parse("2021-09-01T07:00:00"), -20.5F);
-		assertThat(temper1.compareTo(temper2), equalTo(-1));
+		assertTrue("Comparation shall be negative.", temper1.compareTo(temper2) < 0);
 	}
 
 }
