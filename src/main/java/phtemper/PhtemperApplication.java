c255ed47463697d31e14fac3e617beec12e28729
petr-hanc <hancp@centrum.cz>
2021-08-30 12:34:24 +0200

    change default data in DB

--- src/main/java/phtemper/PhtemperApplication.java
+++ src/main/java/phtemper/PhtemperApplication.java
@@ -1,6 +1,9 @@
 package phtemper;
 
 import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.h2.tools.Server;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,7 +32,6 @@ public class PhtemperApplication extends SpringBootServletInitializer implements
 	
 	@Override
 	public void run(String... args) throws Exception {
-		
 		/*
 		repository.deleteAll();
 		List<Temper> tempers = new ArrayList<Temper>();
@@ -37,12 +39,14 @@ public class PhtemperApplication extends SpringBootServletInitializer implements
 		tempers.add(new Temper(LocalDateTime.parse("2021-07-31T04:13:00"), 9.8F));
 		tempers.add(new Temper(LocalDateTime.parse("2021-07-31T09:09:00"), 16.8F));
 		tempers.add(new Temper(LocalDateTime.parse("2021-08-03T05:15:00"), 5.0F));
-		tempers.add(new Temper(LocalDateTime.parse("2021-08-05T13:20:00"), 17F));
-		tempers.add(new Temper(LocalDateTime.parse("2021-08-15T11:40:00"), 17F));
-		tempers.add(new Temper(LocalDateTime.parse("2021-08-20T11:40:00"), 35F));
+		tempers.add(new Temper(LocalDateTime.parse("2021-08-04T08:00:00"), 15.0F));
+		tempers.add(new Temper(LocalDateTime.parse("2021-08-13T12:00:00"), 25.0F));
+		tempers.add(new Temper(LocalDateTime.parse("2021-08-13T12:00:01"), 25.01F));
+		tempers.add(new Temper(LocalDateTime.parse("2021-08-15T13:20:00"), 17F));
+		tempers.add(new Temper(LocalDateTime.parse("2021-08-25T12:00:00"), 17F));
+		tempers.add(new Temper(LocalDateTime.parse("2021-08-27T11:30:00"), 35F));
 		repository.saveAll(tempers);
 		*/
-		
 	}
 		
 	@Bean(destroyMethod = "stop")
