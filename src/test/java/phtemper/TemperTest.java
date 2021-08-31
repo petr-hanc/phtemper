61ef68162ec62940a5a39974c1c606380d6cf001
petr-hanc <hancp@centrum.cz>
2021-08-31 13:25:49 +0200

    TemperTest - add compareTest_bigger()

--- src/test/java/phtemper/TemperTest.java
+++ src/test/java/phtemper/TemperTest.java
@@ -15,4 +15,10 @@ public class TemperTest {
 		assertTrue("Comparation shall be negative.", temper1.compareTo(temper2) < 0);
 	}
 
+	@Test
+	public void compareTest_bigger() {
+		Temper temper1 = new Temper(LocalDateTime.parse("2021-08-31T08:00:00"), -10.5F);
+		Temper temper2 = new Temper(LocalDateTime.parse("2021-09-01T07:00:00"), -20.5F);
+		assertTrue("Comparation shall be positive.", temper2.compareTo(temper1) > 0);
+	}
 }
