333788d58a28712bb653b859ff7e1a9b41820b9f
petr-hanc <hancp@centrum.cz>
2021-09-02 23:26:25 +0200

    added PeriodsControllerIntegrationTest

--- src/test/java/phtemper/api/PeriodsControllerIntegrationTest.java
+++ src/test/java/phtemper/api/PeriodsControllerIntegrationTest.java
@@ -1,11 +1,9 @@
-package phtemper;
+package phtemper.api;
 
-import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,30 +11,38 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import phtemper.Temper;
+import phtemper.TemperRepository;
 
-/** Integration test of PeriodCompute - longestPeriod() and longestPeriodWithTime() */
-
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestPropertySource(locations = "classpath:application-integrationtest.properties")
-public class PeriodComputeIntegrTest {
+
+public class PeriodsControllerIntegrationTest {
 	
     @Autowired
     private TemperRepository repository;
-    @Autowired
-    private PeriodCompute periodCompute;
     @LocalServerPort
     private int port;
     TestRestTemplate restTemplate = new TestRestTemplate();
     HttpHeaders headers = new HttpHeaders();
+    
+    private String createURLWithPort(String uri) {
+        return "http://localhost:" + port + uri;
+    }
     
     @Before
     public void setUp() {
@@ -64,29 +70,26 @@ public class PeriodComputeIntegrTest {
     	Temper temper = new Temper(LocalDateTime.parse("2021-08-14T11:00:00"), 20F);
     	repository.save(temper);
     }
-    
-	@Test
-	public void testLongestPeriodDb() {
-		System.out.println("testLongestPeriod"); // debug
-		System.out.println(repository.findAll());// debug
-		PeriodD period = periodCompute.longestPeriod(15F, 25F);
-		assertThat(period, equalTo(new PeriodD(LocalDate.parse("2021-08-15"), LocalDate.parse("2021-08-25"))));
-	}
 
-	@Test
-	public void testLongestPeriodWithTimeDb() {
-		System.out.println("testLongestPeriodWithTime()");// debug
-		//System.out.println(repository.findAll());// debug
-		PeriodD period = periodCompute.longestPeriodWithTime(15F, 25F, LocalTime.parse("08:00"), LocalTime.parse("12:00"));		
-		assertThat(period, equalTo(new PeriodD(LocalDate.parse("2021-08-04"), LocalDate.parse("2021-08-25"))));
-	}
+    @Test
+    public void testLongestPeriod() throws Exception {
+        HttpEntity<String> entity = new HttpEntity<String>(null, headers);
+        ResponseEntity<String> response = restTemplate.exchange(
+          createURLWithPort("/periods/period?lowTemp=15&hiTemp=25"), HttpMethod.GET, entity, String.class);
+        assertThat(response.getStatusCode(), is(HttpStatus.OK));
+        String expected = "{\"fromDate\":\"2021-08-15\",\"toDate\":\"2021-08-25\"}";
+        System.err.println(response);	// DEBUG
+        JSONAssert.assertEquals(expected, response.getBody(), false);
+    }  
     
-	@Test
-	public void testLongestPeriodDb_2LongestPeriods_olderOne() {
-		prepareTestContext_make2PeriodsSameLength();
-		PeriodD period = periodCompute.longestPeriod(15F, 25F);
-		//System.out.println(period);
-		assertThat(period, equalTo(new PeriodD(LocalDate.parse("2021-08-04"), LocalDate.parse("2021-08-14"))));
-	}
-	
+    @Test
+	public void testLongestPeriodWithTime() throws Exception {
+        HttpEntity<String> entity = new HttpEntity<String>(null, headers);
+        ResponseEntity<String> response = restTemplate.exchange(
+          createURLWithPort("/periods/periodTime?lowTemp=15&hiTemp=25&fromTime=08:00&toTime=12:00"), HttpMethod.GET, entity, String.class);
+        assertThat(response.getStatusCode(), is(HttpStatus.OK));
+        String expected = "{\"fromDate\":\"2021-08-04\",\"toDate\":\"2021-08-25\"}";
+        JSONAssert.assertEquals(expected, response.getBody(), false);
+    }
+
 }
